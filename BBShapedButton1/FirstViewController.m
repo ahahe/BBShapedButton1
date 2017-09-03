3a4aab68a836a6911b39058d19cca74b93e145c1
韦家冰 <weijiabing@swipal.com>
2017-09-03 21:14:47 +0800

    OK

--- BBShapedButton1/FirstViewController.m
+++ BBShapedButton1/FirstViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FirstViewController.h"
+#import "BBShapedButton.h"
+#import "BBIrregularImageButton.h"
 
 @interface FirstViewController ()
 
@@ -16,13 +18,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //XX按钮
+    BBIrregularImageButton *imageBtn = [[BBIrregularImageButton alloc] initWithFrame:CGRectMake(50, 0, 100, 100)];
+    imageBtn.backgroundColor=[UIColor clearColor];
+    [imageBtn setImage:[UIImage imageNamed:@"button-normal"] forState:UIControlStateNormal];
+    [self.view addSubview:imageBtn];
+
+    
+    
+    BBShapedButton *btn = [BBShapedButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(50, 150, 200, 100);
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(0, 100)];
+    [path addLineToPoint:CGPointMake(20, 0)];
+    [path addLineToPoint:CGPointMake(45, 50)];
+    [path addLineToPoint:CGPointMake(70, 0)];
+    [path addLineToPoint:CGPointMake(150, 30)];
+    [path addLineToPoint:CGPointMake(175, 0)];
+    [path addLineToPoint:CGPointMake(200, 100)];
+    [path closePath];
+    btn.path = path;
+    
+    [btn setTitle:@"按钮" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnAction:) forControlEvents:UIControlEventTouchUpInside];
+    btn.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:btn];
+    
+    
+    
+    
 }
-
-
+- (void)btnAction:(UIButton *)sender
+{
+    sender.backgroundColor = [UIColor colorWithRed:(arc4random() % 255 + 1) / 255.0
+                                             green:(arc4random() % 255 + 1) / 255.0
+                                              blue:(arc4random() % 255 + 1) / 255.0
+                                             alpha:1.0];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
+    
 }
 
 
